6ebec1824025eb20f667e342726dd82713e14a04
bourkee3 <32816125+bourkee3@users.noreply.github.com>
2017-12-14 18:30:04 +0000

    Complete nested FOR loop to collect data
    Working nested FOR loop to collect data and place it in a data-frame then into a CSV file. 
    Using the CSV file to create a visualization of the gathered data.

--- Github_Assignment.R
+++ Github_Assignment.R
@@ -20,6 +20,8 @@ githubToken1 <- oauth2.0_token(oauth_endpoints("github"), myapplication1)
 
 getToken1 <- config(token = githubToken1)
 
+#Interrogating the API
+
 # Using user "phadej" to request data and use JSON 
 
 phadejRequest <- GET("https://api.github.com/users/phadej/followers?per_page=100;", getToken1)
@@ -35,12 +37,12 @@ githubDB = jsonlite::fromJSON(jsonlite::toJSON(extract))
 
 # Subset data.frame
 githubDB$login
-githubDB$
 
+# Using user "dowlind1" to request data and use JSON 
 
-request <- GET("https://api.github.com/users/dowlind1/followers?per_page=100;", getToken1)
+request <- GET("https://api.github.com/users/dowlind1/followers", getToken1)
 
-stop_for_status(request)
+#stop_for_status(request)
 
 # Extract content from a request
 extract2 = content(request)
@@ -53,19 +55,21 @@ githubDB2[githubDB2$full_name == "dowlind1", "created_at"]
 githubDB2$login
 
 
-#Want to extract the current users followers and then extract who the current user follows
-#then check if there is duplication or a connection between who the current user follows and their followers
+#Want to extract the current users following and then extract who the current user follows
+#check if there is duplication
 
 #Get current users followers
-#Get who current user follows
-#Convert to database
-#Check for duplication
+#enter for loop to go through following
+#Get who current user of the made vectors following 
+#enter nested for loop to gather information about the user 
+#Collect the data from each loop and place it into database
+
 
 #using a single for loop to gather data from a user ie. who the follow and who their followers follow.
 #including number of repositories commits etc.
 
 
-data <- GET("https://api.github.com/users/sorchaobyrne/following", getToken1)
+data <- GET("https://api.github.com/users/khotyn/following", getToken1)
 data
 extract = content(data)
 dataframe1 = jsonlite::fromJSON(jsonlite::toJSON(extract))
@@ -75,7 +79,7 @@ id
 users = c(id)
 
 #main dataframe with data collected from interrogation of API 
-userDF = data.frame(Username=integer(), Followers = integer(), Repositories = integer())  
+userDF = data.frame(Username=integer(), Followers = integer(), Following=integer(), Repositories = integer(), DateCreated =integer(), UpdatedDate = integer())  
 
 #create empty vectors to hold collected data
 allusers = c()
@@ -83,8 +87,8 @@ allusers = c()
 #totalfollowerslist = c()
 
 
-#FOR loop to gather data from initial user, user hardcoded above 
-
+#FOR loop to gather data from users following vector, user hardcoded above 
+#enter for loop
 for(i in 1:length(users)){
   
   #getting the current users following data using code tested above
@@ -93,47 +97,71 @@ for(i in 1:length(users)){
   url = paste("https://api.github.com/users/", currentusername, "/following",sep = "")
   following = GET( url,getToken1)
   following2 = content(following)
-  followingDF = jsonlite::fromJSON(jsonlite::toJSON(following2))
-  followingloginName = followingDF$login
-  
   
   #if user isnt following anyone move onto next username
   if(length(following2)==0){
     next
   }
+  #placing the users following into a dataframe
+  followingDF = jsonlite::fromJSON(jsonlite::toJSON(following2))
+  followingloginName = followingDF$login
+
   #new for loop to get the followings followers, more data to visualise 
   
   for(j in 1:length(followingloginName)){
     
+    #getting rid of dupicates within the dataframe to complete the network 
     if(is.element(followingloginName[j], allusers) == FALSE){
       
       allusers[length(allusers)+1] = followingloginName[j]
+      
+      #gathering the specific data for each user
+      
       url2 = paste("https://api.github.com/users/", followingloginName[j],sep = "")
       followers = GET( url2,getToken1)
       followers2 = content(followers)
       followersDF = jsonlite::fromJSON(jsonlite::toJSON(followers2))
+      
       #followers
       followerslist = followersDF$followers
+      
+      #following
+      followinglist = followersDF$following
+      
       #repositories
       reposlist = followersDF$public_repos
       
-      userDF[nrow(userDF)+1,]=c(followingloginName[j], followerslist, reposlist)
+      #date account was created
+      datecreated = substr(followersDF$created_at, start = 1, stop = 10)
+      
+      #date account last updated
+      updatedate = substr(followersDF$updated_at, start = 1, stop = 10)
+      
+      #Placing the data into the main dataframe made outside the for loop
+      userDF[nrow(userDF)+1,]=c(followingloginName[j], followerslist, followinglist, reposlist, datecreated, updatedate)
       
     }
-    if(length(allusers) >200){
-      break
+    if(length(allusers) >500){
+      break #creating a limit for collected data
     }
-    print(length(allusers))
     next
   }
-  if(length(allusers) >200){
+  if(length(allusers) >500){
     break
   }
   next
   
 }
 
+#place the dataframe into a csv file 
+
+write.csv(userDF, 'Data.csv')
 
 
+#using plotly
+
+#use the data in the dataframe to visualise the amount of followers verses the number of following 
+#visualise the number of repositories verses the number of repositories that user has
+#show the date which the users account was created on each plot
 
 
